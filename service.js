0d4296187027da6cf23e7470ef3e7906cdb3b7a8
Manuel Gombocz <mgombocz@agilion.net>
2015-08-31 16:43:07 +0200

    Enhanced dragulaService to offer access to currently dragged model.

--- service.js
+++ service.js
@@ -4,98 +4,147 @@ var dragula = require('dragula');
 var dragulaKey = '$$dragula';
 var replicateEvents = require('./replicate-events');
 
-function register (angular) {
-  return [function dragulaService () {
-    return {
-      add: add,
-      find: find,
-      options: setOptions,
-      destroy: destroy
-    };
-    function handleModels(scope, drake){
-      var dragElm;
-      var dragIndex;
-      var dropIndex;
-      var sourceModel;
-      drake.on('remove',function removeModel (el, source) {
-        sourceModel = drake.models[drake.containers.indexOf(source)];
-        scope.$applyAsync(function applyRemove() {
-          sourceModel.splice(dragIndex, 1);
-        });
-      });
-      drake.on('drag',function dragModel (el, source) {
-        dragElm = el;
-        dragIndex = domIndexOf(el, source);
-      });
-      drake.on('drop',function dropModel (dropElm, target, source) {
-        dropIndex = domIndexOf(dropElm, target);
-        scope.$applyAsync(function applyDrop() {
-          sourceModel = drake.models[drake.containers.indexOf(source)];
-          if (target === source) {
-            sourceModel.splice(dropIndex, 0, sourceModel.splice(dragIndex, 1)[0]);
-          } else {
-            var notCopy = dragElm === dropElm;
-            var targetModel = drake.models[drake.containers.indexOf(target)];
-            var dropElmModel = notCopy ? sourceModel[dragIndex] : angular.copy(sourceModel[dragIndex]);
-            
-            if (notCopy) {
-              sourceModel.splice(dragIndex, 1);
-            }
-            targetModel.splice(dropIndex, 0, dropElmModel);
-            target.removeChild(dropElm); // element must be removed for ngRepeat to apply correctly
-          }
-        });
-      });
-    }
-    function getOrCreateCtx (scope) {
-      var ctx = scope[dragulaKey];
-      if (!ctx) {
-        ctx = scope[dragulaKey] = {
-          bags: []
+function register(angular) {
+    return [function dragulaService() {
+
+        var that = this,
+            convertModelFunc = passThroughFunc,
+            draggedModel,
+            droppedModel;
+
+        return {
+            add: add,
+            find: find,
+            options: setOptions,
+            destroy: destroy,
+            draggedModel: getDraggedModel,
+            droppedModel: getDroppedModel,
+            convertModel: convertModel
         };
-      }
-      return ctx;
-    }
-    function domIndexOf(child, parent) {
-      return Array.prototype.indexOf.call(angular.element(parent).children(), child);
-    }
-    function add (scope, name, drake) {
-      var bag = find(scope, name);
-      if (bag) {
-        throw new Error('Bag named: "' + name + '" already exists in same angular scope.');
-      }
-      var ctx = getOrCreateCtx(scope);
-      bag = {
-        name: name,
-        drake: drake
-      };
-      ctx.bags.push(bag);
-      replicateEvents(angular, bag, scope);
-      if(drake.models){ // models to sync with (must have same structure as containers)
-        handleModels(scope, drake);
-      }
-      return bag;
-    }
-    function find (scope, name) {
-      var bags = getOrCreateCtx(scope).bags;
-      for (var i = 0; i < bags.length; i++) {
-        if (bags[i].name === name) {
-          return bags[i];
+        function handleModels(scope, drake) {
+            var dragElm;
+            var dragIndex;
+            var dropIndex;
+            var sourceModel;
+            drake.on('remove', function removeModel(el, source) {
+                sourceModel = drake.models[drake.containers.indexOf(source)];
+                scope.$applyAsync(function () {
+                    sourceModel.splice(dragIndex, 1);
+                });
+            });
+            drake.on('drag', function (el, source) {
+                dragElm = el;
+                dragIndex = domIndexOf(el, source);
+                scope.$applyAsync(function () {
+                    clearModels();
+                    sourceModel = drake.models[drake.containers.indexOf(source)];
+                    that.draggedModel = sourceModel[dragIndex];
+                });
+            });
+            drake.on('cancel', function () {
+                scope.$applyAsync(function () {
+                    clearModels();
+                });
+            });
+
+            drake.on('drop', function (dropElm, target, source) {
+                dropIndex = domIndexOf(dropElm, target);
+                scope.$applyAsync(function applyDrop() {
+                    sourceModel = drake.models[drake.containers.indexOf(source)];
+                    if (target === source) {
+                        sourceModel.splice(dropIndex, 0, sourceModel.splice(dragIndex, 1)[0]);
+                    } else {
+                        var notCopy = dragElm === dropElm;
+                        var targetModel = drake.models[drake.containers.indexOf(target)];
+//                        var dropElmModel = notCopy ? sourceModel[dragIndex] : angular.copy(sourceModel[dragIndex]);
+                        var dropElmModel = notCopy ? that.draggedModel : angular.copy(that.draggedModel);
+                        that.droppedModel = convertModelFunc(dropElmModel);
+
+                        if (notCopy) {
+                            sourceModel.splice(dragIndex, 1);
+                        }
+                        targetModel.splice(dropIndex, 0, that.droppedModel);
+                        target.removeChild(dropElm); // element must be removed for ngRepeat to apply correctly
+                    }
+                });
+            });
         }
-      }
-    }
-    function destroy (scope, name) {
-      var bags = getOrCreateCtx(scope).bags;
-      var bag = find(scope, name);
-      var i = bags.indexOf(bag);
-      bags.splice(i, 1);
-      bag.drake.destroy();
-    }
-    function setOptions (scope, name, options) {
-      var bag = add(scope, name, dragula(options));
-      handleModels(scope, bag.drake);
-    }
-  }];
+
+        function clearModels() {
+            draggedModel = undefined;
+            droppedModel = undefined;
+        }
+
+        function getOrCreateCtx(scope) {
+            var ctx = scope[dragulaKey];
+            if (!ctx) {
+                ctx = scope[dragulaKey] = {
+                    bags: []
+                };
+            }
+            return ctx;
+        }
+
+        function domIndexOf(child, parent) {
+            return Array.prototype.indexOf.call(angular.element(parent).children(), child);
+        }
+
+        function add(scope, name, drake) {
+            var bag = find(scope, name);
+            if (bag) {
+                throw new Error('Bag named: "' + name + '" already exists in same angular scope.');
+            }
+            var ctx = getOrCreateCtx(scope);
+            bag = {
+                name: name,
+                drake: drake
+            };
+            ctx.bags.push(bag);
+            replicateEvents(angular, bag, scope);
+            if (drake.models) { // models to sync with (must have same structure as containers)
+                handleModels(scope, drake);
+            }
+            return bag;
+        }
+
+        function find(scope, name) {
+            var bags = getOrCreateCtx(scope).bags;
+            for (var i = 0; i < bags.length; i++) {
+                if (bags[i].name === name) {
+                    return bags[i];
+                }
+            }
+        }
+
+        function destroy(scope, name) {
+            var bags = getOrCreateCtx(scope).bags;
+            var bag = find(scope, name);
+            var i = bags.indexOf(bag);
+            bags.splice(i, 1);
+            bag.drake.destroy();
+        }
+
+        function setOptions(scope, name, options) {
+            var bag = add(scope, name, dragula(options));
+            handleModels(scope, bag.drake);
+        }
+
+        function passThroughFunc(passedObject) {
+            return passedObject;
+        }
+
+        function convertModel(convertFunc) {
+            that.convertModelFunc = convertFunc || passThroughFunc;
+        }
+
+        function getDraggedModel() {
+            return draggedModel;
+        }
+
+        function getDroppedModel() {
+            return droppedModel;
+        }
+    }];
 }
 
 module.exports = register;
